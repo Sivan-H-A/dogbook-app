0af5adf3adfdf63b86b41a7b21d25aa2466fe446
Sivan-H-A <sivananaf@gmail.com>
2021-05-01 22:56:45 +0300

    create routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import BreedsPage from './pages/BreedsPage/BreedsPage';
+import BreedDetailsPage from './pages/BreedDetailsPage/BreedDetailsPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
-        
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" ><HomePage/></Route>
+          <Route exact path="/breeds"><BreedsPage/></Route>
+          <Route exact path="/breeds/:id"><BreedDetailsPage/></Route>          
+          <Route path="/"><NotFoundPage/></Route>          
+        </Switch>        
+      </HashRouter>
     </div>
   );
 }
