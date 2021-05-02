d60722d2d6a6fc24605f260f59290918df84c6c2
Sivan-H-A <sivananaf@gmail.com>
2021-05-02 21:03:22 +0300

    adding Navbar

---
+++ src/components/BreedsNavbar/BreedsNavbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default function BreedsNavbar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#/">Dog Book</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#/breeds">Breeds</Nav.Link>
+                    {/* {userClick && breedId ? <Nav.Link href="#/breeds/:id">Breed Info</Nav.Link> : null} */}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>    
+    )
+}
