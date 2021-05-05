c78416cd11f7a0c4342a318efea75b055156913c
Sivan-H-A <sivananaf@gmail.com>
2021-05-05 12:47:42 +0300

    handling breeds page getting all breeds and send them to breeds component + adding filter for sending relevant breeds

--- src/pages/BreedsPage/BreedsPage.js
+++ src/pages/BreedsPage/BreedsPage.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { Col, Container, Form, Row, Button } from 'react-bootstrap';
+import Breeds from '../../components/Breeds/Breeds';
+import BreedModel from '../../model/BreedModel';
+import './BreedsPage.css'
 
-export default function BreedsPage() {
+function BreedsPage() {
+    const [breedsList, setBreedsList] = useState([]);
+    const [filterText, setFilterText] = useState("")
+    const [changeImg, setChangeImg] = useState(false);
+
+    useEffect(() => {
+        axios.get("https://dog.ceo/api/breeds/list/all").then(res=>{
+            setBreedsList(Object.keys(res.data.message).map((name, index) => new BreedModel(index,name.charAt(0).toUpperCase() + name.slice(1))));
+        }).catch();
+    }, []);
+
     return (
-        <div>
-            BreedsPage
-        </div>
+        <Container>
+             <Row className="p-breeds">
+                <Col className="col-md-6 col-12">               
+                    <Form.Control type="text" value={filterText} placeholder="Filter by:" onChange={(e)=>setFilterText(e.target.value)}/>
+                </Col>
+                <Col className="col-md-3 col-12">
+                    <Button 
+                        variant="info"
+                        onClick={()=>setChangeImg(!changeImg)}>
+                        Change Imgae</Button>
+                </Col >
+            </Row>
+           <Breeds breedsList={filterText ? breedsList.filter(breed=>breed.name.includes(filterText)): breedsList} changeImg={changeImg}/> 
+        </Container>
     )
 }
+export default BreedsPage;
